d2c6b7f9a994fcbad8ff30c3c133b8d22ac75cb5
witchard <witchard@hotmail.co.uk>
2023-02-09 20:59:05 +0000

    Switch tps for tpw as it conflicts. Add tab completion

--- src/main/java/com/github/witchard/minecraft_teleport/Teleport.java
+++ src/main/java/com/github/witchard/minecraft_teleport/Teleport.java
@@ -4,12 +4,15 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.TabCompleteEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.nio.file.Paths;
 import java.util.logging.Logger;
 
-public final class Teleport extends JavaPlugin {
+public final class Teleport extends JavaPlugin implements Listener {
     private Logger log;
     private State state;
     private final String STATE_FILE = "teleport.state";
@@ -21,16 +24,34 @@ public final class Teleport extends JavaPlugin {
         var path = Paths.get(getDataFolder().getAbsolutePath(), STATE_FILE).toString();
         log.info("loading state from " + path);
         state = State.load(path, log);
+
+        // Register for events
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
     }
 
+    @EventHandler
+    public void onTabComplete(TabCompleteEvent event) {
+        var buffer = event.getBuffer();
+        if (buffer.length() > 3 && (
+                buffer.startsWith("tpd ") ||
+                buffer.startsWith("tpg ") ||
+                buffer.startsWith("tpp ") ||
+                buffer.startsWith("tpa "))) {
+            var prefix = buffer.substring(4);
+            var suggestions = state.teleports.keySet().stream().filter(a -> a.startsWith(prefix)).toList();
+            log.info("Completions: "+suggestions);
+            event.setCompletions(suggestions);
+        }
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (sender instanceof Player p) {
-            if (command.getName().equalsIgnoreCase("tps") && args.length == 1) {
+            if (command.getName().equalsIgnoreCase("tpw") && args.length == 1) {
                 log.info("Saving " + p.getLocation().toString() + " as " + args[0]);
                 state.teleports.put(args[0], p.getLocation());
                 state.save();
